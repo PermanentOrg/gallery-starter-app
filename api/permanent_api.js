6b97057e965aab025841c0018bee1619a776bc16
Philipp Bauer <hello@philippbauer.org>
2018-06-02 14:35:57 -0500

    Added p-shift attribute

--- api/permanent_api.js
+++ api/permanent_api.js
@@ -39,7 +39,6 @@ var permanent = (function () {
     xhr.send();
   }
 
-
   function dataLoaded() {
     checkProfile();
     checkView();
@@ -47,9 +46,11 @@ var permanent = (function () {
 
   function checkView() {
     var repeats = document.querySelectorAll("[p-repeat]");
+    var shifts = document.querySelectorAll("[p-shift]");
     var binds = document.querySelector("[p-bind]");
 
     if (view_container) {
+      // Repeat Properties
       repeats.forEach(function (repeater) {
         var attrval = repeater.getAttribute('p-repeat');
         var scope = attrval.split('in')[1].trim();
@@ -75,6 +76,18 @@ var permanent = (function () {
         repeater.remove();
       });
 
+      // Shift Properties
+      shifts.forEach(function(shift) {
+        var attrval = shift.getAttribute('p-shift');
+        var [scope, target] = attrval.split(' as ');
+        var data = the_data[scope][0];
+
+        if (data) {
+          bindChild(data, shift);
+        } else {
+          throw new Error('Invalid Scope.');
+        }
+      });
     }
   }
 
